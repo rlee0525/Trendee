d566f3c5ce4600c64d2dcc81846b8581e72d97df
Raymond Lee <raylee0525@gmail.com>
2017-03-21 22:32:27 -0700

    Setup responsive screen - splash page, and finish styling splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,6 +5,8 @@ class Splash extends React.Component {
   render() {
     return (
       <div className="splash-container">
+        <img id="splash-img"
+             src="http://res.cloudinary.com/rlee0525/image/upload/c_scale,w_1864/v1490159250/splash_b6w94i.png" />
         <SessionFormContainer />
       </div>
     )
