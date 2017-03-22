289447cbbabf967fc6e58fc9c2fc6bad8f10d769
Raymond Lee <raylee0525@gmail.com>
2017-03-22 16:55:37 -0700

    Added Intro logo

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,6 +5,9 @@ class Splash extends React.Component {
   render() {
     return (
       <div className="splash-container">
+        <div id="splash-logo">
+          <p>Set and discover the</p><p id="splash-title">trend.</p>
+        </div>
         <img id="splash-img"
              src="http://res.cloudinary.com/rlee0525/image/upload/c_scale,w_1864/v1490159250/splash_b6w94i.png" />
         <SessionFormContainer />
