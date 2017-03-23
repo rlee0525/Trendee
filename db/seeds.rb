69b732722635e05ddcd3d579de8ba949ad9e4c32
Raymond Lee <raylee0525@gmail.com>
2017-03-22 22:31:31 -0700

    Update seed file to destroy all first

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
 
 uid1 = User.create!(username: "RaymondLee", password: "password", email: "raylee0525@gmail.com", first_name: "Raymond", last_name: "Lee")
