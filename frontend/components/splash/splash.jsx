40b34c448aecd14570c3dd2abaca346b9100e1cc
Raymond Lee <raylee0525@gmail.com>
2017-03-24 12:24:01 -0700

    Update photo

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -9,7 +9,7 @@ class Splash extends React.Component {
           <p>Set and discover the</p><p id="splash-title">trend.</p>
         </div>
         <img id="splash-img"
-             src="http://res.cloudinary.com/rlee0525/image/upload/v1490341700/Slice-min_thmg9g.png" />
+             src="http://res.cloudinary.com/rlee0525/image/upload/v1490383382/Slice-min_k3ontp.png" />
         <SessionFormContainer />
       </div>
     )
